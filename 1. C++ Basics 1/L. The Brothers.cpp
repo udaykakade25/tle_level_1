e0d1661926caa771c0c962273847e0e14f5f0c74
Uday Kakade <141299403+udaykakade25@users.noreply.github.com>
2025-05-21 22:35:26 +0530

    Create L. The Brothers.cpp

---
+++ 1. C++ Basics 1/L. The Brothers.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+       
+    string s,ss, a, aa;
+    cin >> s >> ss >> a >> aa;
+    
+    if(ss == aa) cout << "ARE Brothers" << endl;
+    else cout << "NOT" << endl; 
+    
+}
