52dd473099004422e468431f65deeb950ea7e467
Uday Kakade <141299403+udaykakade25@users.noreply.github.com>
2025-05-22 15:00:06 +0530

    Create N. Char.cpp

---
+++ 1. C++ Basics 1/N. Char.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	char a;
+	cin >> a;
+	char b = toupper(a);
+	
+	if(a == b) cout << char(tolower(a)) << endl;
+	else cout << char(toupper(a)) << endl;
+	
+	
+	
+	
+}
